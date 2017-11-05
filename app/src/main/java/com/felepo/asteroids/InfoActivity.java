f0c7f2571d614431ab7afe81a204d05c6832cd39
Fernando Poncio <fecrackle@gmail.com>
2017-11-04 20:08:34 -0600

    Creando InfoActivity InfoActivity es una actividad que muestra información sobre la aplicación.

---
+++ app/src/main/java/com/felepo/asteroids/InfoActivity.java
@@ -0,0 +1,17 @@
+package com.felepo.asteroids;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Fernando Poncio on 4/11/2017.
+ */
+
+public class InfoActivity extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
--- app/src/main/java/com/felepo/asteroids/MainActivity.java
+++ app/src/main/java/com/felepo/asteroids/MainActivity.java
@@ -1,7 +1,9 @@
 package com.felepo.asteroids;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Método para iniciar una actividad diferente al MainActivity
+    protected void ejectutarInfo( View view )
+    {
+        //Se crea una intención para decir que la nueva actividad se ejectuta desde
+        //ActivityMain(this) hacía InfoActivity
+        Intent i = new Intent(this, InfoActivity.class);
+        startActivity(i);
     }
 }
