2a657f68bb15dd3ed12c385d8d390c9a1c7d23a4
Fernando Poncio <fecrackle@gmail.com>
2017-11-07 00:07:33 -0600

    Agregando Menu principal en ActivityMain

--- app/src/main/java/com/felepo/asteroids/MainActivity.java
+++ app/src/main/java/com/felepo/asteroids/MainActivity.java
@@ -3,6 +3,8 @@ package com.felepo.asteroids;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Método para iniciar una actividad diferente al MainActivity
-    protected void ejectutarInfo( View view )
+    protected void ejecutarInfo( View view )
     {
         //Se crea una intención para decir que la nueva actividad se ejectuta desde
         //ActivityMain(this) hacía InfoActivity
@@ -26,5 +28,35 @@ public class MainActivity extends AppCompatActivity {
     public void salirApp(View view)
     {
         finish();
+    }
+
+    //Código para activar el menú XML
+    @Override
+    public boolean onCreateOptionsMenu(Menu miMenu)
+    {
+        getMenuInflater().inflate(R.menu.menu_activity, miMenu);
+
+        return true;
+    }
+
+    //Establecer acciones al pulsar algún botón
+    @Override
+    public boolean onOptionsItemSelected(MenuItem opcion_menu)
+    {
+        int id = opcion_menu.getItemId();
+
+        if( id == R.id.configuracion )
+        {
+            return true;
+        }
+
+        if( id == R.id.info )
+        {
+            ejecutarInfo(null);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(opcion_menu);
     }
 }
